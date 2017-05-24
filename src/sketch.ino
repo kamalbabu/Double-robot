2923889f94ad28da5cf1127b9b2fa0627c214071
yashdeep97 <yashdeep97@gmail.com>
2017-05-24 14:21:34 +0530

    added arduino code and libraries

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,4 +1,4 @@
-    #include <AFMotor.h>
+     #include <AFMotor.h>
      
     AF_DCMotor motor1(1, MOTOR12_64KHZ); // create motor #2, 64KHz pwm
     AF_DCMotor motor2(2, MOTOR12_64KHZ);
@@ -15,63 +15,84 @@
     }
      
     void loop() {
-      Serial.print("tick");
-      forward();
-      delay(2000);
+	int x;
+      //Serial.print("tick");
+      //forward();
+      /*delay(2000);
       backward();
       delay(2000);
-      right();
-      delay(2000);
-      left();
-      delay(2000);
-   
-
-    }
+      */
+	delay(100);
+while(!Serial.available()>0){
+}
+	x = Serial.read();
+	delay(500);
+x-=48;
+	Serial.print(x);
+switch(x){
+	case 1:
+			forward();
+			delay(100);
+			break;
+	
+	case 2:	
+			backward();
+			delay(100);
+			break;
+	case 3:	
+			left();
+			delay(100);
+			break;
+	case 4:	
+			right();
+			delay(100);
+			break;
+	default:	stop();
+}
+      }
 
    void forward(){
 
-      motor1.run(FORWARD);      // turn it on going forward
-      delay(100);
+      motor1.run(FORWARD);      // turn it on going forward 
       motor2.run(BACKWARD);      // turn it on going forward
-      delay(100);
-      motor3.run(FORWARD);      // turn it on going forward
-      delay(100);
-      motor4.run(BACKWARD);      // turn it on going forward
-      delay(100);
-     
+       motor3.run(FORWARD);      // turn it on going forward
+       motor4.run(BACKWARD);
+	delay(2000);
+	stop();      // turn it on going forward
+      
    }
 
    void backward(){
    
       motor1.run(BACKWARD);      // turn it on going forward
-      delay(100);
       motor2.run(FORWARD);      // turn it on going forward
-      delay(100);
       motor3.run(BACKWARD);      // turn it on going forward
-      delay(100);
-      motor4.run(FORWARD);      // turn it on going forward
-      delay(100);
+      motor4.run(FORWARD);
+	delay(2000);
+	stop();      // turn it on going forward
    }
 
    void right(){
       motor1.run(FORWARD);      // turn it on going forward
-      delay(100);
       motor2.run(BACKWARD);      // turn it on going forward
-      delay(100);
       motor3.run(BACKWARD);      // turn it on going forward
-      delay(100);
-      motor4.run(FORWARD);      // turn it on going forward
-      delay(100);
+      motor4.run(FORWARD);
+	delay(500);
+	stop();      // turn it on going forwar
    }
 
   void left(){
      motor1.run(BACKWARD);      // turn it on going forward
-      delay(100);
       motor2.run(FORWARD);      // turn it on going forward
-      delay(100);
       motor3.run(FORWARD);      // turn it on going forward
-      delay(100);
-      motor4.run(BACKWARD);      // turn it on going forward
-      delay(100);
+      motor4.run(BACKWARD);
+	delay(500);
+	stop();      // turn it on going forward
   }
+ void stop(){
+	motor1.run(RELEASE);
+	motor2.run(RELEASE);
+	motor3.run(RELEASE);
+	motor4.run(RELEASE);
+}
 
